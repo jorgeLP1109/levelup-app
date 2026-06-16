c8721b579578ebdfe9fa4bb9dbdb8608201533dc
jorgeLP1109 <sofitech@me.com>
2026-06-15 22:20:40 -0400

    prod: apuntar app a api.levelupsportctg.com

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,13 +1,7 @@
-import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 
 class AppConstants {
-  static String get apiBaseUrl {
-    if (Platform.isAndroid) {
-      return 'http://10.0.2.2:5000/api';
-    }
-    return 'http://localhost:5000/api';
-  }
+  static const String apiBaseUrl = 'https://api.levelupsportctg.com/api';
 
   static const String appName = 'Level Up';
 }
